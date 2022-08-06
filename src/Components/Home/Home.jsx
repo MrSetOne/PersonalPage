aacaec4123244527e8a12a7011d019557fec6ebb
MrSetOne <lara.sanchez.michael@gmail.com>
2022-08-06 14:28:00 +0200

    preFramerMotion

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.scss";
+import img1 from "../../Assets/1.png";
+
+const Home = () => {
+  return (
+    <section className="Home">
+      <div className="Home__info">
+        <h1>Michael Lara</h1>
+        <h2>FullStack Developer</h2>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
