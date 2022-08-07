3f2e1ecf0d76fb302fb8e0ecf7002851b9586eff
MrSetOne <lara.sanchez.michael@gmail.com>
2022-08-07 20:03:57 +0200

    wellcomer

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,50 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "./Home.scss";
-import img1 from "../../Assets/1.png";
+import img1 from "../../Assets/1.svg";
+import img2 from "../../Assets/2.svg";
+import img3 from "../../Assets/3.svg";
 
 const Home = () => {
   return (
     <section className="Home">
-      <div className="Home__info">
+      <motion.div
+        className="Home__info"
+        initial={{ opacity: 0, x: -40 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.7, delay: 1.2 }}
+      >
         <h1>Michael Lara</h1>
         <h2>FullStack Developer</h2>
-      </div>
+      </motion.div>
+      <motion.img
+        src={img1}
+        className="Home__Deco Home__Deco--tl"
+        initial={{ opacity: 0, rotate: 20, y: -50, x: -30 }}
+        animate={{ opacity: 1, y: 0, x: 0 }}
+        transition={{ duration: 0.7, delay: 1 }}
+      />
+      <motion.img
+        src={img3}
+        className="Home__Deco Home__Deco--tr"
+        initial={{ opacity: 0, rotate: 5, y: -50, x: 80 }}
+        animate={{ opacity: 1, y: 0, x: 0 }}
+        transition={{ duration: 0.7, delay: 1 }}
+      />
+      <motion.img
+        src={img1}
+        className="Home__Deco Home__Deco--br"
+        initial={{ opacity: 0, rotate: 20, y: 50, x: 30 }}
+        animate={{ opacity: 1, y: 0, x: 0 }}
+        transition={{ duration: 0.7, delay: 1 }}
+      />
+      <motion.img
+        src={img2}
+        className="Home__Deco Home__Deco--bl"
+        initial={{ opacity: 0, rotate: 5, y: 50, x: -80 }}
+        animate={{ opacity: 1, y: 0, x: 0 }}
+        transition={{ duration: 0.7, delay: 1 }}
+      />
     </section>
   );
 };
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,42 +1,59 @@
 import React from "react";
 import "./NavBar.scss";
+import { motion } from "framer-motion";
 
 const NavBar = ({ viewSys }) => {
   return (
     <nav className="NavBar">
       <ul>
-        <li>
+        <motion.li
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <button
             style={{ fontWeight: viewSys.view === "Inicio" ? 700 : 400 }}
             onClick={() => viewSys.setView("Inicio")}
           >
             Inicio
           </button>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.25 }}
+        >
           <button
             style={{ fontWeight: viewSys.view === "Sobre mi" ? 700 : 400 }}
             onClick={() => viewSys.setView("Sobre mi")}
           >
             Sobre mi
           </button>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+        >
           <button
             style={{ fontWeight: viewSys.view === "Portfolio" ? 700 : 400 }}
             onClick={() => viewSys.setView("Portfolio")}
           >
             Portfolio
           </button>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.75 }}
+        >
           <button
             style={{ fontWeight: viewSys.view === "Contacto" ? 700 : 400 }}
             onClick={() => viewSys.setView("Contacto")}
           >
             Contacto
           </button>
-        </li>
+        </motion.li>
       </ul>
     </nav>
   );
