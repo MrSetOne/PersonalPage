7b2af4ef6af143816380312f1e95184949d536a1
MrSetOne <lara.sanchez.michael@gmail.com>
2022-09-29 18:45:44 +0200

    provision transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import AboutMe from "./Components/AboutMe/AboutMe";
 import Contact from "./Components/Contact/Contact";
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -5,12 +5,14 @@ import "./Contact.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { AnimatePresence, motion } from "framer-motion";
+import bg from "../../Assets/4.svg";
 
 const serviceID = process.env.REACT_APP_SERVICE_ID;
 const templateID = process.env.REACT_APP_YOUR_TEMPLATE_ID;
 const publicKey = process.env.REACT_APP_PUBLIC_KEY;
 
-const Contact = () => {
+const Contact = ({ status, viewSys, setExiting }) => {
   const [submiteable, setSubmiteable] = useState(false);
   const [data, setData] = useState({ name: "", email: "", message: "" });
 
@@ -34,89 +36,168 @@ const Contact = () => {
   };
 
   return (
-    <section className="Contact">
-      <div className="Contact__Card">
-        <header>
-          <h2>¡Hablemos!</h2>
-          <p>
-            Actualmente estoy en busqueda activa de mi primera oportunidad como
-            desarrollador frontEnd o FullStack. Si buscas a alguien con mi
-            perfil no dudes en contactarme a través de este formulario. !Estaré
-            encantado de escuchar tu oferta!
-          </p>
-        </header>
-        <form onSubmit={(e) => sendMail(e)}>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Tu nombre"
-            onChange={handleInputChange}
+    <AnimatePresence>
+      {!status && (
+        <section className="Contact">
+          <motion.div
+            className="Contact__Card"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{
+              scale: 1,
+              opacity: 1,
+              transition: { duration: 0.6, delay: 0.3 },
+            }}
+            exit={{ opacity: 0, y: 50 }}
+          >
+            <header>
+              <motion.h2
+                initial={{ opacity: 0, y: 50 }}
+                animate={{
+                  y: 0,
+                  opacity: 1,
+                  transition: { delay: 0.9, duration: 0.3 },
+                }}
+                exit={{ opacity: 0, y: 50 }}
+              >
+                ¡Hablemos!
+              </motion.h2>
+              <motion.p
+                initial={{ opacity: 0, y: 50 }}
+                animate={{
+                  y: 0,
+                  opacity: 1,
+                  transition: { delay: 1.1, duration: 0.3 },
+                }}
+                exit={{ opacity: 0, y: 50 }}
+              >
+                Actualmente estoy en busqueda activa de mi primera oportunidad
+                como desarrollador frontEnd o FullStack. Si buscas a alguien con
+                mi perfil no dudes en contactarme a través de este formulario.
+                !Estaré encantado de escuchar tu oferta!
+              </motion.p>
+            </header>
+            <motion.form
+              onSubmit={(e) => sendMail(e)}
+              initial={{ opacity: 0, y: 50 }}
+              animate={{
+                y: 0,
+                opacity: 1,
+                transition: { delay: 1.3, duration: 0.3 },
+              }}
+              exit={{ opacity: 0, y: 50 }}
+            >
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Tu nombre"
+                onChange={handleInputChange}
+              />
+              <input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="Tu email"
+                onChange={handleInputChange}
+              />
+              <textarea
+                rows={8}
+                name="message"
+                id="message"
+                placeholder="Tu mensaje"
+                onChange={handleInputChange}
+              ></textarea>
+              <div>
+                <a href={cv} download="CV_Michael_Lara">
+                  Descargar CV
+                </a>
+                <button type="submit" disabled={!submiteable}>
+                  Enviar
+                </button>
+              </div>
+            </motion.form>
+            <div className="Contact__More">
+              <motion.header
+                initial={{ opacity: 0, y: 50 }}
+                animate={{
+                  y: 0,
+                  opacity: 1,
+                  transition: { delay: 1.5, duration: 0.3 },
+                }}
+                exit={{ opacity: 0, y: 50 }}
+              >
+                <div></div>
+                <h3>Tambien puedes contarcarme en</h3>
+                <div></div>
+              </motion.header>
+            </div>
+            <ul>
+              <li>
+                <motion.a
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{
+                    y: 0,
+                    opacity: 1,
+                    transition: { delay: 1.7, duration: 0.3 },
+                  }}
+                  exit={{ opacity: 0, y: 50 }}
+                  target={"_blank"}
+                  rel="noreferrer"
+                  href="mailto: lara.sanchez.michael@gmail.com"
+                >
+                  <FontAwesomeIcon icon={faEnvelope} />
+                  Email
+                </motion.a>
+              </li>
+              <li>
+                <motion.a
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{
+                    y: 0,
+                    opacity: 1,
+                    transition: { delay: 1.8, duration: 0.3 },
+                  }}
+                  exit={{ opacity: 0, y: 50 }}
+                  target={"_blank"}
+                  rel="noreferrer"
+                  href="https://www.linkedin.com/in/michaellaras/"
+                >
+                  <FontAwesomeIcon icon={faLinkedin} />
+                  Linkedin
+                </motion.a>
+              </li>
+              <li>
+                <motion.a
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{
+                    y: 0,
+                    opacity: 1,
+                    transition: { delay: 1.9, duration: 0.3 },
+                  }}
+                  exit={{ opacity: 0, y: 50 }}
+                  target={"_blank"}
+                  rel="noreferrer"
+                  href="https://github.com/MrSetOne"
+                >
+                  <FontAwesomeIcon icon={faGithubSquare} />
+                  Github
+                </motion.a>
+              </li>
+            </ul>
+          </motion.div>
+          <motion.img
+            initial={{ y: -50, opacity: 0, rotate: "180deg" }}
+            animate={{
+              y: 0,
+              opacity: 1,
+              transition: { duration: 0.7 },
+            }}
+            src={bg}
+            alt="background"
           />
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="Tu email"
-            onChange={handleInputChange}
-          />
-          <textarea
-            rows={10}
-            name="message"
-            id="message"
-            placeholder="Tu mensaje"
-            onChange={handleInputChange}
-          ></textarea>
-          <div>
-            <a href={cv} download="CV_Michael_Lara">
-              Descargar CV
-            </a>
-            <button type="submit" disabled={!submiteable}>
-              Enviar
-            </button>
-          </div>
-        </form>
-        <div className="Contact__More">
-          <header>
-            <div></div>
-            <h3>Tambien puedes contarcarme en</h3>
-            <div></div>
-          </header>
-        </div>
-        <ul>
-          <li>
-            <a
-              target={"_blank"}
-              rel="noreferrer"
-              href="mailto: lara.sanchez.michael@gmail.com"
-            >
-              <FontAwesomeIcon icon={faEnvelope} />
-              Email
-            </a>
-          </li>
-          <li>
-            <a
-              target={"_blank"}
-              rel="noreferrer"
-              href="https://www.linkedin.com/in/michaellaras/"
-            >
-              <FontAwesomeIcon icon={faLinkedin} />
-              Linkedin
-            </a>
-          </li>
-          <li>
-            <a
-              target={"_blank"}
-              rel="noreferrer"
-              href="https://github.com/MrSetOne"
-            >
-              <FontAwesomeIcon icon={faGithubSquare} />
-              Github
-            </a>
-          </li>
-        </ul>
-      </div>
-    </section>
+        </section>
+      )}
+    </AnimatePresence>
   );
 };
 
